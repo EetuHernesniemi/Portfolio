7e572eee9cfc312ef03c6e7a46690d1452b10401
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2020-10-16 21:21:11 +0300

    better test description

--- portfolio-client-app/src/app/app.component.spec.ts
+++ portfolio-client-app/src/app/app.component.spec.ts
@@ -16,16 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-client-app'`, () => {
+  it(`should have a variable by the name of web_page_title with value 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-client-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('portfolio-client-app app is running!');
+    expect(app.web_page_title).toEqual('portfolio');
   });
 });
