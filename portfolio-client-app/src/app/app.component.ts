3eefa8d1959770f08956b926c33ceaab6254a9e4
Eetu Hernesniemi <eetu.hernesniemi@hotmail.com>
2020-10-16 20:45:20 +0300

    title and name variable changes

--- portfolio-client-app/src/app/app.component.ts
+++ portfolio-client-app/src/app/app.component.ts
@@ -13,7 +13,9 @@ export class AppComponent {
   web_page_owner_last_name = 'hernesniemi';
   web_page_owner_full_name = this.web_page_owner_first_name + " " + this.web_page_owner_last_name;
   public constructor(private titleService: Title) {
-    const title: string = this.web_page_title.charAt(0).toUpperCase + " | " + this.web_page_owner_first_name.charAt(0).toUpperCase + " " + this.web_page_owner_last_name.toUpperCase;
+    const title: string = this.web_page_title.charAt(0).toUpperCase() + this.web_page_title.slice(1) + " | "
+      + this.web_page_owner_first_name.charAt(0).toUpperCase() + this.web_page_owner_first_name.slice(1)
+      + " " + this.web_page_owner_last_name.charAt(0).toUpperCase() + this.web_page_owner_last_name.slice(1);
     this.setTitle(title);
   }
 
